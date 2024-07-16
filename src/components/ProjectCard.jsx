f3d19ef28733065584ba89b8203c056aa6b60f34
Bishesh Shrestha <bisheshshrestha7@gmail.com>
2024-07-16 21:29:34 +0200

    heroicons

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/24/outline";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 const ProjectCard = ({ imgUrl, title, description }) => {
